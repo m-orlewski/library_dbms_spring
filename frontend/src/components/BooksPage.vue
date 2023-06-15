f2ef23ea97e579cab1cbe859f9189e14557f7db1
morlewski <morlewski@student.agh.edu.pl>
2023-06-16 01:08:49 +0200

    refactor

--- frontend/src/components/BooksPage.vue
+++ frontend/src/components/BooksPage.vue
@@ -2,10 +2,15 @@
     <div>
         <h2>Lista książek</h2>
 
+        <b-button block id="show-btn" @click="showCreateModal">
+            <b-icon-plus class="text-white"></b-icon-plus>
+            <span class="h6 text-white">Dodaj książkę</span>
+        </b-button>
+
         <b-table :items="books" :fields="fields" stacked="md" responsive="sm" striped hover fixed>
             <template #cell(actions)="data">
-              <b-button size="sm" @click="editBook(data)" class="mx-1">Edytuj</b-button>
-              <b-button size="sm" variant="danger" @click="deleteBook(data)" class="mx-1">Usuń</b-button>
+              <b-button size="sm" @click="showUpdateModal(data.item.id)" class="mx-1">Edytuj</b-button>
+              <b-button size="sm" variant="danger" @click="showDeleteModal(data.item.id)" class="mx-1">Usuń</b-button>
         </template>
         </b-table>
     </div>
@@ -20,7 +25,32 @@ let BOOKS_API_URL = 'http://localhost:8085/api/books';
 export default {
     data() {
         return {
-            fields: [{key:'id', label:'ID', sortable: true}, {key:'title', label:'Tytuł', sortable: true}, {key:'author', label:'Autor', sortable: true}, {key:'genre', label:'Gatunek', sortable: true}, {key:'quantity', label:'Ilość egzemplarzy'},{key: 'actions'}],
+            fields: [
+                {
+                    key:'id',
+                    label:'ID', 
+                    sortable: true
+                },
+                {
+                    key:'title',
+                    label:'Tytuł', 
+                    sortable: true
+                },
+                {
+                    key:'author',
+                    label:'Autor',
+                    sortable: true},
+                {
+                    key:'genre',
+                    label:'Gatunek',
+                    sortable: true},
+                {
+                    key:'quantity',
+                    label:'Ilość egzemplarzy'
+                },
+                {
+                    key: 'actions',
+                }],
             books: [],
         };
     },
@@ -34,9 +64,14 @@ export default {
                 console.log(error);
             })
         },
-        editBooks() {
+        showCreateModal() {
+            console.log('Create Form');
         },
-        deleteBook() {
+        showUpdateModal(id) {
+            console.log('Update element with id=' + id);
+        },
+        showDeleteModal(id) {
+            console.log('Delete element with id=' + id);
         },
     },
     created() {
