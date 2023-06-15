cdd7c9751339b8f89e536d59370663cf60c21d04
morlewski <morlewski@student.agh.edu.pl>
2023-06-15 19:15:49 +0200

    init frontend

---
+++ frontend/src/components/HeaderBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="primary">
+    <b-container>
+      <b-row class="text-white">
+        <b-col>
+          <span class="h6">System Zarządzania Bazą Danych Biblioteki</span>
+        </b-col>
+      </b-row>
+    </b-container>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "HeaderBar",
+};
+</script>
