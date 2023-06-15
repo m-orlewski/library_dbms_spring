c96a96e840aaceb16d662a67efe8de973612ac60
morlewski <morlewski@student.agh.edu.pl>
2023-06-16 00:51:58 +0200

    added cross origin

--- backend/src/main/java/com/dev/backend/controller/BookController.java
+++ backend/src/main/java/com/dev/backend/controller/BookController.java
@@ -10,6 +10,7 @@ import com.dev.backend.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Klasa definiująca endpointy dotyczące tabeli z książkami i ich zachowanie 
  */
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class BookController {
 	/**
